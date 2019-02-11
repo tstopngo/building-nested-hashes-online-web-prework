b908d3f2ffdd8623afcf12c052382dc882d4d214
Tracy Ngo <tngo1206@gmail.com>
2019-02-11 04:03:36 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,16 +2,16 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriach => {},
-      :matriach {}
-      :hero {},
-      :hero_friends{}
+      :matriach => {}
+      :hero => {},
+      :hero_friends => {}
       }, 
     :capulet => {
       #your key/value pairs here!
-      :patriach {},
-      :matriach {}
-      :heroine {},
-      :heroine_friends{}
+      :patriach => {},
+      :matriach => {}
+      :heroine => {},
+      :heroine_friends => {}
       }}
 
   
